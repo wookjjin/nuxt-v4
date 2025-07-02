8aa57eccecd6e3ed910cc02d73ff4254ffb34209
JinWook <jwook0318@gmail.com>
2025-07-02 13:25:47 +0900

    feat(setting): change directory path

---
+++ app/layouts/auth.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="flex flex-col h-screen justify-center items-center">
+    <slot/>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
